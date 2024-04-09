4ade5e9cf6f1aa99e1a6b1c6203c7e2088a5bc0e
amoako419 <ghheskey@gmail.com>
2024-04-09 23:23:57 +0000

    initial commit

--- cafetaria/lib/main.dart
+++ cafetaria/lib/main.dart
@@ -27,11 +27,10 @@ class TabBarDemo extends StatelessWidget {
             ),
             title: const Text('Cafeteria'),
           ),
-          body: const TabBarView(
+          body:  TabBarView(
        children: [
-        
-          Center(child: Image(image: AssetImage("assets/1.JPG"))),
-          Center(child: Text("Jollof,TZ,Waakye")),
+          _monday(),
+          _tuesday(),
           Center(child: Text("koko,Braised Rice,Fufu")),
           Center(child: Text("Rice,Banku,Waakye")),
           Center(child: Text("Rice,Banku,Waakye")),
@@ -45,4 +44,27 @@ class TabBarDemo extends StatelessWidget {
     
     );
   }
+ 
+}
+Widget _monday(){
+  return  Column(
+    children: [
+      const Image(image: AssetImage("assets/1.JPG",),height: 200.0),
+      const Text("waakye"),
+      ElevatedButton(onPressed: () {}, child: Text("Order"))
+    ],
+  );
+}
+
+// Tuesday
+
+Widget _tuesday(){
+  return  Column(
+    children: [
+      const Image(image: AssetImage("assets/2.JPG",),height: 200.0),
+      const Text("Kenkey with Fried fish"),
+      ElevatedButton(onPressed: () {}, child: const Text("Order"))
+    ],
+    
+  );
 }

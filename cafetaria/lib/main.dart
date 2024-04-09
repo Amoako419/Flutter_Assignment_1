7c64b9bc54ad3a1ebf8a3f888b7f4b1fda7130e9
amoako419 <ghheskey@gmail.com>
2024-04-09 23:32:01 +0000

    initial commit

--- cafetaria/lib/main.dart
+++ cafetaria/lib/main.dart
@@ -31,7 +31,7 @@ class TabBarDemo extends StatelessWidget {
        children: [
           _monday(),
           _tuesday(),
-          Center(child: Text("koko,Braised Rice,Fufu")),
+         _wednesday(),
           Center(child: Text("Rice,Banku,Waakye")),
           Center(child: Text("Rice,Banku,Waakye")),
           Center(child: Text("Rice,Banku,Waakye")),
@@ -51,7 +51,11 @@ Widget _monday(){
     children: [
       const Image(image: AssetImage("assets/1.JPG",),height: 200.0),
       const Text("waakye"),
-      ElevatedButton(onPressed: () {}, child: Text("Order"))
+      ElevatedButton(onPressed: () {}, child: const Text("Order")),
+
+      const Image(image: AssetImage("assets/10.JPG",),height: 200.0),
+      const Text("Banku with egg stew"),
+      ElevatedButton(onPressed: () {}, child: const Text("Order")),
     ],
   );
 }
@@ -63,7 +67,27 @@ Widget _tuesday(){
     children: [
       const Image(image: AssetImage("assets/2.JPG",),height: 200.0),
       const Text("Kenkey with Fried fish"),
-      ElevatedButton(onPressed: () {}, child: const Text("Order"))
+      ElevatedButton(onPressed: () {}, child: const Text("Order")),
+
+       const Image(image: AssetImage("assets/6.JPG",),height: 200.0),
+      const Text("Jollof with Chicken "),
+      ElevatedButton(onPressed: () {}, child: const Text("Order")),
+      
+    ],
+    
+  );
+}
+Widget _wednesday(){
+  return  Column(
+    children: [
+      const Image(image: AssetImage("assets/5.WEBP",),height: 200.0),
+      const Text("Yam and Plantain ampesi with plavan sauce"),
+      ElevatedButton(onPressed: () {}, child: const Text("Order")),
+
+       const Image(image: AssetImage("assets/11.JPG",),height: 200.0),
+      const Text("Jollof with Chicken "),
+      ElevatedButton(onPressed: () {}, child: const Text("Order")),
+      
     ],
     
   );
